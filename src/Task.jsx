f0935217977981a772d1e8e85ad1fe7f51ac4344
MOHAMMED THOUFEEK A <www.thoufeekabc@gmail.com>
2023-03-17 19:41:11 +0530

    final

--- src/Task.jsx
+++ src/Task.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export const Task = (props) => {
   return (
-    <div className='task' 
-    style={{backgroundColor:props.taskComplete?"green":"white"}}>
+    <div className='task' style={{backgroundColor:props.taskComplete?"green":"white"}}>
     <h1 style={{}}>{props.taskName}</h1>
     <button onClick={()=>props.complete(props.taskId)}>Complete</button>
     <button onClick={()=>props.deleteList(props.taskId)}>Delete</button>
